9a26c97251180006a69cad998b46d3a48ad20a5b
Moureen Hamutenya <69241623+mou-rush@users.noreply.github.com>
2021-02-18 21:19:47 +0530

    File Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,9 @@
 import React from 'react';
-import { Grid, TextField, Button, InputAdornment} from "@material-ui/core"
-import {AccountCircle, LockRounded} from "@material-ui/icons"
+import LoginPage from './components/LoginPage';
 
-const App: React.FC =() => {
+const App: React.FC = () => {
   return (
-    <div>
-<Grid container style={{minHeight: '100vh'}}>
-<Grid item xs={12} sm={6}>
-<img src="https://images.unsplash.com/photo-1525197134674-4c3f02da2745?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8NnNNVmpUTFNrZVF8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" 
-style={{width:"100%", height: "100%"}} 
-alt="brand" />
-</Grid>
-
-<Grid container item xs={12} sm={6} alignItems="center" 
-direction="column"
-justify-content="space-between" 
-style={{padding: 10}}> 
- <div/>
- <div style={{display: 'flex', flexDirection:"column",
- maxWidth: 400,
- minWidth: 300,
- }}
- >
-<Grid container justify="center" >
-<img src="https://images.unsplash.com/photo-1515552638994-8ce3dc5fea97?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80" 
-width={200} 
-alt="logo" 
-/>
-</Grid>
-<TextField 
-label="username" 
-margin="normal" 
-InputProps={{startAdornment: <InputAdornment position='start'><AccountCircle/></InputAdornment>}}/>
-<TextField 
-type="password"
-label="password" margin="normal" 
-InputProps={{startAdornment: <InputAdornment position='start'><LockRounded/></InputAdornment>}}/>
-<div style={{height:20}}/>
-<Button color="primary" variant="contained">
-Log in
-</Button>
-<div style={{height:20}}/>
-<Button>Interested in joining?</Button>
-</div>
-<div />
-
-</Grid>
-</Grid>
-</div>
+    <LoginPage />
   );
 }
 
